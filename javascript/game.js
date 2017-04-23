3a4f6d088cb547c4bbdd9f1635c421d09a25cac5
Chirag K Shah <chiragkshah@Chirags-MacBook-Pro.local>
2017-04-23 18:48:29 -0400

    selecting emeny

--- javascript/game.js
+++ javascript/game.js
@@ -5,14 +5,87 @@ var $player;
 var $enemy;
 var characterHandles = ["finn", "rey", "poe", "kylo", "bb8"];
 var enemyHandles = [];
+var enemyHandle;
 var currentEnemy;
-function setupGame() {
+
+	// Player: Rey
+	var rey = {
+		name:"Rey",
+		shortName:"Rey",
+		// - Health Points
+		healthPoints: 100,
+		// - Attack Power
+		attackPower: 5, 
+		// - Base Attack Power
+		baseAttackPower: 5,
+		// - Counter Attack Power
+		counterAttackPower: 10,
+		//image url
+		image: "images/rey.png"
+	}
+	// Player: Kylo
+	var kylo = {
+		name:"Kylo Ren",
+		shortName:"Kylo",
+		// - Health Points
+		healthPoints: 100,
+		// - Attack Power
+		attackPower: 5, 
+		// - Base Attack Power
+		baseAttackPower: 5,
+		// - Counter Attack Power
+		counterAttackPower: 10,
+		//image url
+		image: "images/kylo.png"
+	}
+	// Player: Finn
+	var rey = {
+		name:"Finn",
+		shortName:"Finn",
+		// - Health Points
+		healthPoints: 100,
+		// - Attack Power
+		attackPower: 5, 
+		// - Base Attack Power
+		baseAttackPower: 5,
+		// - Counter Attack Power
+		counterAttackPower: 10,
+		//image url
+		image: "images/Finn.png"
+	}
+	// Player: Poe
+	var kylo = {
+		name:"Poe Dameron",
+		shortName:"Poe",
+		// - Health Points
+		healthPoints: 100,
+		// - Attack Power
+		attackPower: 5, 
+		// - Base Attack Power
+		baseAttackPower: 5,
+		// - Counter Attack Power
+		counterAttackPower: 10,
+		//image url
+		image: "images/poe.png"
+	}
+		// Player: BB8
+	var rey = {
+		name:"BB-8",
+		shortName:"BB8",
+		// - Health Points
+		healthPoints: 100,
+		// - Attack Power
+		attackPower: 5, 
+		// - Base Attack Power
+		baseAttackPower: 5,
+		// - Counter Attack Power
+		counterAttackPower: 10,
+		//image url
+		image: "images/bb8.png"
+	}
 
 
-
-}
-
-
+var characters = [];
 
 $(document).ready(function(){
 
@@ -37,14 +110,16 @@ $(document).ready(function(){
 	 		//update character with player style
 	 		$player.addClass("player");
 
+
 	 		//update enemies and move them
 	 		for(var i =0; i<characterHandles.length; i ++ ){
 	 			if(characterHandles[i] !== playerHandle){
 	 				var charClass = "." + characterHandles[i];
+	 				enemyHandles.push(charClass);
 	 				var $newEnemy = $(charClass);
 
 	 				//remove from charactes section
-	 				console.log("removing: " + charClass);
+	 				console.log("detaching: " + charClass);
 	 				$newEnemy.detach();
 	 				// remove character class
 //	 				$newEnemy.removeClass("character");
@@ -64,9 +139,13 @@ $(document).ready(function(){
 	 	}
 
 	 	else if ($(this).hasClass("enemy")) {
+
+	 		if(!enemySelected){
 	 		//create a reference to the enemy
 	 		$enemy = $(this);
-			console.log("enemy detected! : " + $enemy.attr("data-name"));
+	 		//store enemy name...
+	 		enemyHandle = $enemy.attr("data-name");
+			console.log("enemy detected! : " + enemyHandle);
 			
 			// remove it from enemies area
 			$enemy.detach();
@@ -76,7 +155,10 @@ $(document).ready(function(){
 
 			//set boolean to true to allow attackl
 			enemySelected = true;
-
+			}
+			else {
+				alert("you've already selected an enemy; please press the attack button to start your attack!");
+			}
 
 
 	 	}
@@ -84,25 +166,25 @@ $(document).ready(function(){
 	 });
 
 	 $(".attack").click(function(){
-	  	if (playerSelected && enemySelected){
-	  		alert("attack!");
-	  	}
-	  	else {
-	  		if(!playerSelected && !enemySelected) alert("need to select a player and enemy!");
-	  		else if( playerSelected && !enemySelected) {
-	  			alert("need to select an enemy!");
-	  			console.log("player: " + $player.attr("data-name"));
-	  		
-	  		}
 
+	  		if(!playerSelected && !enemySelected) alert("need to select a player and enemy!");
+	  		else if( playerSelected && !enemySelected) alert("need to select an enemy!");
 
 	  		else if( !playerSelected && enemySelected) alert("something's wrong....");
 	  		else {
 	  			//we're all good to attack...
-	  			alert("we're all good to attack...");
+	  			console.log("ATTACk!....");
+	  			attack();
 	  		}
-	  	}
+	  	
 	 });
+
+function attack(){
+	console.log("attack function");
+
+//	$enemy.healthpoints -= healthoints;
+	
+}
 
 
 
