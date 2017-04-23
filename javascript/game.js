dc8fc2291462b73569b56a73cb4959e2f898692a
Chirag K Shah <chiragkshah@Chirags-MBP.home>
2017-04-23 07:20:47 -0400

    Enemies to attack

--- javascript/game.js
+++ javascript/game.js
@@ -1,23 +1,15 @@
-
-
+var playerSelected = false;
+var enemySelected = false;
+var playerHandle;
+var $player;
+var $enemy;
+var characterHandles = ["finn", "rey", "poe", "kylo", "bb8"];
+var enemyHandles = [];
+var currentEnemy;
 function setupGame() {
 
-	var character = {
-		name:"Obi-Wan",
-		// - Health Points
-		healthPoints: 100,
-		// - Attack Power
-		attackPower: 6, 
-		// - Base Attack Power
-		baseAttackPower: 6,
-		// - Counter Attack Power
-		counterAttackPower: 10
-	}
 
-	var $player = $('#player');
-	$player.append(character.name);
-	$player.append("<br>");
-	$player.append(character.healthPoints);
+
 }
 
 
@@ -25,5 +17,71 @@ function setupGame() {
 $(document).ready(function(){
 
 	console.log("Document Ready function");
-	setupGame();
+	//setupGame();
+
+	 $(".character").click(function(){
+		console.log("character on click");
+		console.log();
+
+
+	 	if (!playerSelected){
+	 		//user is selecting a player
+	 		$player = $(this);
+	 		console.log("player selected: " + $player.attr("data-name"));
+	 		playerHandle = $player.attr("data-name");
+	 		//update character with player style
+	 		$player.addClass("player");
+
+	 		//update enemies and move them
+	 		for(var i =0; i<characterHandles.length; i ++ ){
+	 			if(characterHandles[i] !== playerHandle){
+	 				var charClass = "." + characterHandles[i];
+	 				var $newEnemy = $(charClass);
+
+	 				//remove from charactes section
+	 				console.log("removing: " + charClass);
+	 				$newEnemy.remove();
+	 				// remove character class
+	 				$newEnemy.removeClass("character");
+
+	 				// add enemy class and formating;
+	 				$newEnemy.addClass("enemy");
+
+	 				//add to enemies section
+	 				$("#enemy").append($newEnemy);
+	 			}
+
+	 		}
+
+	 		//set playerSelected flag
+	 		playerSelected = true;
+
+	 	}
+
+	 });
+
+	 $(".attack").click(function(){
+	  	if (playerSelected && enemySelected){
+	  		alert("attack!");
+	  	}
+	  	else {
+	  		if(!playerSelected && !enemySelected) alert("need to select a player and enemy!");
+	  		else if( playerSelected && !enemySelected) alert("need to select an enemy!");
+	  		else if( !playerSelected && enemySelected) alert("something's wrong....");
+	  		else {
+	  			//we're all good to attack...
+	  			alert("we're all good to attack...");
+	  		}
+	  	}
+	 });
+
+	 // $(".enemy").click(function(){
+	 // 	alert("enemy clicked");
+	 // });
+
 });
+
+
+
+
+
